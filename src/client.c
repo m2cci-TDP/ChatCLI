bc70e35283083dcb8da31689e415a5cf84f8ce8e
m2cci-VGU <vince.guiu@gmail.com>
2019-02-08 09:27:32 +0100

    changed for local run

--- src/client.c
+++ src/client.c
@@ -66,12 +66,13 @@ int main(int argc, char *argv[])
 /* procedure correspondant au traitement du client de votre application */
 void client_appli (char *serveur, char *service)
 {
-	int noSocket = h_socket(AF_INET, SOCK_DGRAM);
+	int noSocket = h_socket(AF_UNIX, SOCK_DGRAM);
 	struct sockaddr_in *p_addrSocket;
 	adr_socket(service, serveur, SOCK_DGRAM, &p_addrSocket);
 	h_bind(noSocket, p_addrSocket);
 	char message[BUFFER_SIZE+1];
 	for(int i = 0; i < 10; i++) {
+
 		sprintf(message, "Salut mon loulou, je suis le message %d :)", i);
 		h_sendto(noSocket, message, BUFFER_SIZE, p_addrSocket);
 	}
